e9bfa9927fcbdfb6c7c957660cff78473b32322e
Michael Milewski <saramic@gmail.com>
2021-10-02 19:25:51 +1000

    first attempt js arrays with elyses-enchantments

---
+++ javascript/elyses-enchantments/enchantments.js
@@ -0,0 +1,105 @@
+// @ts-check
+
+/**
+ * Retrieve card from cards array at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ *
+ * @returns {number} the card
+ */
+export function getItem(cards, position) {
+  return cards[position];
+}
+
+/**
+ * Exchange card with replacementCard at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ * @param {number} replacementCard
+ *
+ * @returns {number[]} the cards with the change applied
+ */
+export function setItem(cards, position, replacementCard) {
+  return [
+    ...cards.slice(0, position),
+    replacementCard,
+    ...cards.slice(position + 1, cards.length),
+  ];
+}
+
+/**
+ * Insert newCard at the end of the cards array
+ *
+ * @param {number[]} cards
+ * @param {number} newCard
+ *
+ * @returns {number[]} the cards with the newCard applied
+ */
+export function insertItemAtTop(cards, newCard) {
+  cards.push(newCard);
+  return cards;
+}
+
+/**
+ * Remove the card at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItem(cards, position) {
+  return [
+    ...cards.slice(0, position),
+    ...cards.slice(position + 1, cards.length),
+  ];
+}
+
+/**
+ * Remove card from the end of the cards array
+ *
+ * @param {number[]} cards
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItemFromTop(cards) {
+  return [...cards.slice(0, cards.length - 1)];
+}
+
+/**
+ * Insert newCard at beginning of the cards array
+ *
+ * @param {number[]} cards
+ * @param {number} newCard
+ *
+ * @returns {number[]} the cards including the new card
+ */
+export function insertItemAtBottom(cards, newCard) {
+  cards.unshift(newCard);
+  return cards;
+}
+
+/**
+ * Remove card from the beginning of the cards cards
+ *
+ * @param {number[]} cards
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItemAtBottom(cards) {
+  return [...cards.slice(1, cards.length)];
+}
+
+/**
+ * Compare the number of cards with the given stackSize
+ *
+ * @param {number[]} cards
+ * @param {number} stackSize
+ *
+ * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
+ */
+export function checkSizeOfStack(cards, stackSize) {
+  return cards.length === stackSize;
+}
