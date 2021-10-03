b1b2e43b97bad5fe7d9113649c033bca4f90c8dd
Michael Milewski <saramic@gmail.com>
2021-10-03 19:24:54 +1100

    1st go at JS looping elyses-looping-enchantments

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,26 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  return stack.filter((stackCard) => stackCard === card).length;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  // NOTE: maybe this is more obvious?
+  // return stack.filter((stackCard) => stackCard % 2 === (type ? 0 : 1)).length;
+  return stack.filter((stackCard) => (stackCard % 2 === 0) === type).length;
+}
