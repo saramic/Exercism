d91b266bdd0c97e4e02c26884c798cde7d38d6fb
Michael Milewski <saramic@gmail.com>
2023-03-29 15:49:40 +1100

    Update to accumulate ?

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,8 +1,8 @@
-=begin
-Write your code for the 'Accumulate' exercise in this file. Make the tests in
-`accumulate_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/accumulate` directory.
-=end
-
+# extend array class with accumulate
+class Array
+  def accumulate(&block)
+    map{ block.call(_1) }
+  end
+end
--- ruby/accumulate/accumulate_test.rb
+++ ruby/accumulate/accumulate_test.rb
@@ -3,11 +3,11 @@ require_relative 'accumulate'
 
 class ArrayTest < Minitest::Test
   def test_empty_accumulation
-    assert_equal [], [].accumulate { |e| e * e }
+    assert_equal [], ([].accumulate { |e| e * e })
   end
 
   def test_accumulate_squares
-    skip
+    # skip
     result = [1, 2, 3].accumulate do |number|
       number * number
     end
@@ -15,29 +15,29 @@ class ArrayTest < Minitest::Test
   end
 
   def test_accumulate_upcases
-    skip
-    result = %w(hello world).accumulate(&:upcase)
-    assert_equal %w(HELLO WORLD), result
+    # skip
+    result = %w[hello world].accumulate(&:upcase)
+    assert_equal %w[HELLO WORLD], result
   end
 
   def test_accumulate_reversed_strings
-    skip
-    result = %w(the quick brown fox etc).accumulate(&:reverse)
-    assert_equal %w(eht kciuq nworb xof cte), result
+    # skip
+    result = %w[the quick brown fox etc].accumulate(&:reverse)
+    assert_equal %w[eht kciuq nworb xof cte], result
   end
 
   def test_accumulate_recursively
-    skip
-    result = %w(a b c).accumulate do |char|
-      %w(1 2 3).accumulate do |digit|
+    # skip
+    result = %w[a b c].accumulate do |char|
+      %w[1 2 3].accumulate do |digit|
         "#{char}#{digit}"
       end
     end
-    assert_equal [%w(a1 a2 a3), %w(b1 b2 b3), %w(c1 c2 c3)], result
+    assert_equal [%w[a1 a2 a3], %w[b1 b2 b3], %w[c1 c2 c3]], result
   end
 
   def test_do_not_change_in_place
-    skip
+    # skip
     original = [1, 2, 3]
     copy = original.dup
     original.accumulate { |n| n * n }
